0e2a8a0723dfadc6ee5d3ede2da1f761e2e1d2c3
NDisponible <cursowebweb22@gmail.com>
2023-03-21 16:55:46 +0100

    subiendo proyecto

---
+++ index.js
@@ -0,0 +1,35 @@
+const API_KEY = 'f09ff28913b5d381ddcf6e6a76128245';
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => setWeatherData(data))
+}
+const setWeatherData = data => {
+    console.log(data);
+    const weatherData = {
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperature: data.main.temp,
+        date: getDate(),
+    }
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key];
+    });
+    cleanUp();
+}
+const cleanUp = () => {
+    let container = document.getElementById('container');
+    let loader = document.getElementById('loader');
+    loader.style.display = 'none';
+    container.style.display = 'flex';
+}
+const getDate = () => {
+    let date = new Date();
+    return `${date.getDate()}-${('0' + (date.getMonth() +1)).slice(-2)}-${date.getFullYear()}`;
+}
+const onLoad = () => {
+    navigator.geolocation.getCurrentPosition(fetchData);
+}
